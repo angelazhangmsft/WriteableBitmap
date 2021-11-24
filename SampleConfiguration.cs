1a437ed9a1c0ca198c1361ed3d117c2cba8b98eb
Angela Zhang <angzhang@microsoft.com>
2021-11-23 17:07:03 -0800

    Add project files.

---
+++ SampleConfiguration.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using System.Collections.Generic;
+
+namespace WriteableBitmap
+{
+    internal static class SampleConfig
+    {
+        public const string FeatureName = "WriteableBitmap";
+        public static ElementTheme CurrentTheme = ElementTheme.Default;
+    }
+
+    public partial class MainPage : Page
+    {
+        private readonly List<Scenario> scenarios = new()
+        {
+            new Scenario() { Title = "Scenario 1", ClassName = typeof(Scenario1_ShortName).FullName },
+        };
+    }
+
+    public class Scenario
+    {
+        public string Title { get; set; }
+        public string ClassName { get; set; }
+    }
+}
